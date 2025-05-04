aca31923d87f00c32fa097220bce527e0c8ba651
AliAbdullah0314 <jafris12345@gmail.com>
2025-05-04 18:33:45 +0400

    updated requirements and settings

--- f1predictor/f1predictor/settings.py
+++ f1predictor/f1predictor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g(y^q&r6mtc+&@!mbqfb-fecigyt22=#)+mi2u-2^7!iukn-tf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
